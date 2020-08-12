be136a96fafe079249e7685055624fe9521417e9
Paul Smith <prsmith@ranken.edu>
2020-08-11 20:42:22 -0500

    removing routes for front-end packages

--- server.js
+++ server.js
@@ -29,9 +29,6 @@ app.use(compression());
 app.get('/', (request, response) => {
   response.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-app.use('/jquery/js', express.static('node_modules/jquery/dist'));
-app.use('/bootstrap', express.static('node_modules/bootstrap/dist'));
-app.use('/bootswatch', express.static('node_modules/bootswatch/dist'));
 app.use('/dist', express.static('dist'));
 app.use(express.static('public'));
 app.use((request, response) => {
